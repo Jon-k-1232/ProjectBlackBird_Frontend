d10aa22218de27db74ccd3f9cfaabd20ab046372
Jon Kimmel <jonkimmel16@gmail.com>
2021-12-29 23:15:47 -0700

    working on contact_card

---
+++ src/components/ContactCard/ContactCard.js
@@ -0,0 +1,144 @@
+import React, { useState } from 'react';
+import { Typography, Card, CardContent } from '@mui/material';
+import './ContactCard.css';
+import greenCheckmark from '../../Static_Icons/greenCheckmark.svg';
+import redCircle from '../../Static_Icons/redCircle.svg';
+import EllipsisMenu from '../EllipsisMenu/EllipsisMenu';
+
+export default function ContactCard(props) {
+  const {
+    address1,
+    address2,
+    balanceChanged,
+    beginningBalance,
+    city,
+    companyName,
+    currentBalance,
+    email,
+    fax,
+    firstName,
+    inactive,
+    lastName,
+    mobilePhone,
+    newBalance,
+    notBillable,
+    oid,
+    phoneNumber1,
+    phoneNumber2,
+    state,
+    statementBalance,
+    zip
+  } = props.data;
+
+  const [edit, setEdit] = useState(false);
+
+  const basicContactInformation = () => (
+    <table className="contactColumnOne">
+      <tr>
+        <th>Company Name:</th>
+        <td>{companyName}</td>
+      </tr>
+      <tr>
+        <th>Name:</th>
+        <td>
+          {lastName}, {firstName}
+        </td>
+      </tr>
+      <tr>
+        <th>Primary Address:</th>
+        <td>
+          {address1} {city}, {state} {zip}
+        </td>
+      </tr>
+      {address2 && (
+        <tr>
+          <th>Secondary Address:</th>
+          <td>
+            {address2} {city}, {state} {zip}
+          </td>
+        </tr>
+      )}
+      <tr>
+        <th>Cell:</th>
+        <td>{mobilePhone}</td>
+      </tr>
+      <tr>
+        <th>Landline:</th>
+        <td>{phoneNumber1}</td>
+      </tr>
+      {phoneNumber2 && (
+        <tr>
+          <th>Secondary Landline:</th>
+          <td>{phoneNumber2}</td>
+        </tr>
+      )}
+      <tr>
+        <th>Fax:</th>
+        <td>{fax}</td>
+      </tr>
+      <tr>
+        <th>Email:</th>
+        <td>{email}</td>
+      </tr>
+    </table>
+  );
+
+  const additionalDetail = () => (
+    <table className="contactColumnTwo">
+      <tr>
+        <th>Beginning Balance:</th>
+        <td>{beginningBalance}</td>
+      </tr>
+      <tr>
+        <th>Statement Balance:</th>
+        <td>{statementBalance}</td>
+      </tr>
+      <tr>
+        <th>Current Balance:</th>
+        <td>{currentBalance}</td>
+      </tr>
+      <tr>
+        <th>New Balance:</th>
+        {conditionalCheckboxs(newBalance)}
+      </tr>
+
+      <tr>
+        <th>Balance Changed Since Last Statement:</th>
+        {conditionalCheckboxs(balanceChanged)}
+      </tr>
+      <tr>
+        <th>Billable:</th>
+        {conditionalCheckboxs(!notBillable)}
+      </tr>
+    </table>
+  );
+
+  const conditionalCheckboxs = (value) => (
+    <td>
+      {value ? (
+        <img src={greenCheckmark} alt="Green Checkmark" />
+      ) : (
+        <img src={redCircle} alt="Red Negative Circle" />
+      )}
+    </td>
+  );
+
+  return (
+    <Card className="contactWrapper">
+      <CardContent style={styles()} className="contactHeader">
+        <Typography variant="h3">{companyName}</Typography>
+        <Typography variant="subtitle1">{inactive ? 'Inactive' : 'Active'}</Typography>
+        <Typography variant="subtitle1">Client:{oid}</Typography>
+        <EllipsisMenu edit={setEdit} menuOptions={['Edit']} />
+      </CardContent>
+      <CardContent style={styles()} className="contactTables">
+        {basicContactInformation()}
+        {additionalDetail()}
+      </CardContent>
+    </Card>
+  );
+}
+
+const styles = () => ({
+  padding: '0px'
+});
--- src/pages/DashboardApp.js
+++ src/pages/DashboardApp.js
@@ -1,12 +1,15 @@
 import { Box, Container, Typography } from '@mui/material';
+import ContactCard from '../components/ContactCard/ContactCard';
 import Page from '../components/Page';
+import dummyContact from '../_mocks_/contactCard_mock';
 
 export default function DashboardApp() {
   return (
-    <Page title="Dashboard | Minimal-UI">
+    <Page title="Dashboard">
       <Container maxWidth="xl">
         <Box sx={{ pb: 5 }}>
-          <Typography variant="h4">Hi, Welcome back</Typography>
+          {/* <Typography variant="h4">Hi, Welcome back</Typography> */}
+          <ContactCard data={dummyContact} />
         </Box>
       </Container>
     </Page>
